330ad3993092b8bc9b65b248d0e9fdb4dedb68f6
teemotinn <grossotimoteo@gmail.com>
2023-08-08 20:12:19 -0300

    prepara-sqlite

--- App.js
+++ App.js
@@ -3,9 +3,22 @@ import { useFonts } from 'expo-font'
 import Navigator from './src/navigation/Navigator'
 import { Provider } from 'react-redux'
 import store from './src/store/store'
-
+import { dropTableSessions, init } from './src/SQLite';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  useEffect(()=> {
+    init()
+      .then((result)=> {
+        console.log('Db initialized/dropped')
+        console.log(result);
+      })
+      .catch(err => {
+        console.log("Initialization DB failed:");
+        console.log(err.message);
+    })
+  }, [])
 
   const [fontsLoaded] = useFonts({
     'Nunito': require('./src/assets/fonts/Nunito/Nunito-Regular.ttf'),
