520c53e3978ce5fdda2673e458a39739968d5b97
teemotinn <grossotimoteo@gmail.com>
2023-09-04 21:02:11 -0300

    agrega-descripcion-snackbar

--- src/components/MySnackbar.jsx
+++ src/components/MySnackbar.jsx
@@ -3,6 +3,17 @@ import React from "react";
 import { Snackbar } from "react-native-paper";
 import { colors } from "../global/colors";
 
+/**
+ * MySnackbar component for displaying a Snackbar notification.
+ *
+ * @param {object} props - Component props.
+ * @param {boolean} props.isVisible - Determines the visibility of the Snackbar.
+ * @param {string} props.message - The message text to display in the Snackbar.
+ * @param {function} props.onDismiss - Callback function when the Snackbar is dismissed.
+ * @param {function} props.onPressAction - Callback function when the "Ok" action button is pressed.
+ * @returns {JSX.Element} - MySnackbar component.
+ */
+
 export default function MySnackbar({
     isVisible,
     message,
